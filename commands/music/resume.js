ed3d4554f5a8fecc3b6a2caafd3182f57d759916
KarimX32 <ikakarim9@gmail.com>
2021-03-06 10:37:19 +0200

    Part 2

---
+++ commands/music/resume.js
@@ -0,0 +1,16 @@
+const discord = require('discord.js')
+const distube = require('distube')
+
+module.exports.run = async (Client, message, args, prefix) => {
+    if(!message.content.startsWith(prefix)) return
+    if(!message.member.voice.channel) return message.reply('please join a vc before using this cmd')
+    const queue = Client.distube.getQueue(message)
+    if(!queue) return message.channel.send('There is nothing playing')
+    Client.distube.resume(message)
+    message.channel.send('Song is resumed')
+}
+
+module.exports.help = {
+    name: 'resume',
+    aliases: []
+}
